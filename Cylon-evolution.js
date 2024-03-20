e4ce5a6779c580e33a170c4b2832c0fef538c2cd
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-03-20 13:08:31 -0700

    Update Cylon-evolution.js

--- Cylon-evolution.js
+++ Cylon-evolution.js
@@ -12,3 +12,21 @@ which will return the string "Infiltrate the colonies".
 
 
 // Solution
+
+let Cylon = function (model){
+  this.model = model;
+}
+
+Cylon.prototype.attack = function() {
+  return "Destroy all humans!";
+};
+
+let HumanSkin = function (model) {
+  Cylon.apply(this, arguments);
+}
+
+HumanSkin.prototype = Object.create(Cylon.prototype);
+HumanSkin.prototype.constructor = HumanSkin;
+HumanSkin.prototype.infiltrate = function() {
+  return "Infiltrate the colonies";
+}
